bc5dd3f7f2bca72f56af869b6d743e1499f8c848
SauravSharma <saurav.sharma@infostride.com>
2022-01-31 13:20:11 +0530

    Automation of OrangeHrm using inheritance..

--- src/test/java/Inheri.java
+++ src/test/java/Inheri.java
@@ -11,20 +11,17 @@ import java.util.Scanner;
 
 class Parent{
     WebDriver driver = new ChromeDriver();
-    String txtUsername = "Admin";
-    String txtPassword = "admin123";
     String firstName = RandomStringUtils.randomAlphabetic(8);
     String lastName = RandomStringUtils.randomAlphabetic(8);
     String employeeID = RandomStringUtils.randomNumeric(4);
     String fullName = firstName + lastName;
     Select drag;
-
-    void login() {
+    void login(String uname,String pass) {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://opensource-demo.orangehrmlive.com/");
-        driver.findElement(By.id("txtUsername")).sendKeys(txtUsername);
-        driver.findElement(By.id("txtPassword")).sendKeys(txtPassword);
+        driver.findElement(By.id("txtUsername")).sendKeys(uname);
+        driver.findElement(By.id("txtPassword")).sendKeys(pass);
         driver.findElement(By.name("Submit")).click();
     }
     void recruit(String da,String dat) throws InterruptedException {
@@ -45,12 +42,12 @@ class Parent{
         driver.findElement(By.id("candidateSearch_toDate")).sendKeys(dat);
         driver.findElement(By.id("btnSrch")).click();
     }
-    void adduser() {
+    void adduser(String fname, String lname, String empId) {
         driver.findElement(By.id("menu_pim_viewPimModule")).click();
         driver.findElement(By.id("btnAdd")).click();
-        driver.findElement(By.id("firstName")).sendKeys(firstName);
-        driver.findElement(By.id("lastName")).sendKeys(lastName);
-        driver.findElement(By.id("employeeId")).sendKeys(employeeID);
+        driver.findElement(By.id("firstName")).sendKeys(fname);
+        driver.findElement(By.id("lastName")).sendKeys(lname);
+        driver.findElement(By.id("employeeId")).sendKeys(empId);
         driver.findElement(By.id("photofile")).sendKeys("C:\\Users\\SauravSharma\\" +
                 "OneDrive - InfoStride Inc\\Pictures\\logo.jpg");
         JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -66,7 +63,7 @@ class Parent{
     }
     void edit() {
         driver.findElement(By.id("menu_admin_viewAdminModule")).click();
-        driver.findElement(By.id("searchSystemUser_userName")).sendKeys(txtUsername);
+        driver.findElement(By.id("searchSystemUser_userName")).sendKeys();
         driver.findElement(By.id("searchBtn")).click();
         driver.findElement(By.xpath("//*[contains(@href,'saveSystemUser?userId=1')]")).click();
         driver.findElement(By.id("btnSave")).click();
@@ -91,18 +88,41 @@ class Parent{
 }
 class Inheri extends Parent{
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Enter search from date as YYYY-MM-DD");
+        //For login
+        System.out.println("Enter username");
         Scanner obj=new Scanner(System.in);
-        String date=obj.nextLine();
-        System.out.println("Enter search to date as YYYY-MM-DD");
-        String dat=obj.nextLine();
+        String uname=obj.nextLine();
+        System.out.println("Enter password");
+        String pass=obj.nextLine();
+
+        //For recruit(Calender)
+//        System.out.println("Enter search from date as YYYY-MM-DD");
+//        Scanner obj=new Scanner(System.in);
+//        String date=obj.nextLine();
+//        System.out.println("Enter search to date as YYYY-MM-DD");
+//        String dat=obj.nextLine();
+
+        //for adding new user
+//        System.out.println("Enter firstname");
+//        Scanner obj=new Scanner(System.in);
+//        String fname=obj.nextLine();
+//        System.out.println("Enter lasttname");
+//        Scanner obje=new Scanner(System.in);
+//        String lname=obj.nextLine();
+//        System.out.println("Enter Id");
+//        Scanner objec=new Scanner(System.in);
+//        String id=obj.nextLine();
+//        System.out.println("Enter fullname");
+//        Scanner object=new Scanner(System.in);
+//        String fullname=obj.nextLine();
+
         WebDriverManager.chromedriver().setup();
         Parent abc = new Parent();
-        abc.login();
-        abc.adduser();
-        abc.recruit(date,dat);
-        abc.edit();
-        abc.delete();
+        abc.login(uname,pass);
+        //abc.adduser(fname, lname, empId);
+        //abc.recruit(date,dat);
+        //abc.edit();
+        //abc.delete();
         abc.logout();
     }
 }
